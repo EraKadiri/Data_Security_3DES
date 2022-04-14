72d78ab8e2a3d6b746b2c11cf5a325783d9db8f6
EraKadiri <78500922+EraKadiri@users.noreply.github.com>
2022-04-15 00:17:51 +0200

    Created TripleDes class and added Encrypt and Decrypt methods

---
+++ 3DES/3DES/TripleDes.cs
@@ -0,0 +1,18 @@
+﻿
+namespace _3DES
+{
+    public class TripleDes
+    {
+        private const string securityKey = "DataSecurity3DES";
+
+        public static string Encrypt(string TextToEncrypt)
+        {
+            //TODO: implement method
+        }
+
+        public static string Decrypt(string TextToDecrypt)
+        {
+            //TODO: implement method
+        }
+    }
+}
