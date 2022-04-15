1ba437e46408b8d7e490652f79b190e82acc39f9
EraKadiri <78500922+EraKadiri@users.noreply.github.com>
2022-04-15 15:02:01 +0200

    Updates triple des and fixed return value

--- 3DES/3DES/TripleDes.cs
+++ 3DES/3DES/TripleDes.cs
@@ -12,7 +12,32 @@ namespace _3DES
             string encryptedText = "";
             byte[] MyEncryptedArray = UTF8Encoding.UTF8.GetBytes(TextToEncrypt);
             MD5CryptoServiceProvider MyMD5CryptoService = new MD5CryptoServiceProvider();
-            return encryptedText;
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash
+              (UTF8Encoding.UTF8.GetBytes(securityKey));
+
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new
+               TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var MyCryptoTransform = MyTripleDESCryptoService
+               .CreateEncryptor();
+
+            byte[] MyresultArray = MyCryptoTransform
+               .TransformFinalBlock(MyEncryptedArray, 0,
+               MyEncryptedArray.Length);
+
+            MyTripleDESCryptoService.Clear();
+
+            return Convert.ToBase64String(MyresultArray, 0,
+               MyresultArray.Length); ;
         }
 
         public static string Decrypt(string TextToDecrypt)
