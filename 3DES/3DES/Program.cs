5567af2a1311f4e2ef3261713021de15315c67a1
erezaismajlii <erezaismajli@gmail.com>
2022-04-15 02:48:40 +0200

    Added repeat option

--- 3DES/3DES/Program.cs
+++ 3DES/3DES/Program.cs
@@ -16,6 +16,14 @@ namespace _3DES
                 Console.WriteLine("Please only answer with 'text' or 'file'");
                 input = Console.ReadLine();
             }
+            EncryptDecrypt(input);
+            Console.WriteLine("Do you want to repeat (y/n): ");
+            string continueInput = Console.ReadLine();
+            while (continueInput.ToLower() == "y")
+            {
+                Console.WriteLine("Do you want to encrypt text or file?: ");
+                input = Console.ReadLine();
+                input = input.ToLower();
         }
     }
 }
