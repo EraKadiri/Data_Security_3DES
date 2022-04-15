fe2f5ee7291fb582b0ad35c5c7b36e05bf73f343
erezaismajlii <erezaismajli@gmail.com>
2022-04-15 02:45:54 +0200

    Updated Program.cs and Encrypt method in Program class

--- 3DES/3DES/Program.cs
+++ 3DES/3DES/Program.cs
@@ -7,6 +7,15 @@ namespace _3DES
         static void Main(string[] args)
         {
             string encryptedText = TripleDES.Encrypt("hello world");
+            Console.WriteLine("Do you want to encrypt text or file?: ");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Please only answer with 'text' or 'file'");
+                input = Console.ReadLine();
+            }
         }
     }
 }
